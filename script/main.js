2dcfbdbbc7e872812aede0ef07a94aab6427eb89
Alexandra Lem <a_lem@fanshaweonline.ca>
2022-03-05 13:56:58 -0500

    drag and drop

--- script/main.js
+++ script/main.js
@@ -1,7 +1,9 @@
 (() => {
 	// collect the buttons at the bottom of the page
 	let bottomNav = document.querySelectorAll("#buttonHolder img"),
-			gameBoard = document.querySelector(".puzzle-board");
+			gameBoard = document.querySelector(".puzzle-board"),
+			puzzlePieces = document.querySelectorAll(".puzzle-pieces img"),
+			dropZones = document.querySelectorAll(".drop-zone");
 
 	/* bottomNav collects all of the image elements into an array-like container that looks like this
 	[
@@ -20,6 +22,43 @@
 		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgref}.jpg)`
 	}
 
+	function dragStarted(event) {
+		console.log('started dragging a piece');
+		// use the setData method of the drag event to store a reference to the current element
+		event.dataTransfer.setData('currentItem', event.target.id);
+	}
+
+	function allowDragOver(event) {
+		// turn off the default browser behaviour -> we want toa llow a dragover
+		event.preventDefault();
+		console.log('dragged over me');
+		// retrieve the dragged element using dataTransfer object
+		// this was set in the drag event using the set data method
+		let droppedEl = event.dataTransfer.getData('currentItem');
+		console.log(droppedEl);
+
+		// appendChild is a built-in js function that adds an element to another as a child
+		// move the dragged element into the dropzone
+		// appendChild is a built-in js function that adds an element to another as a child
+		this.appendChild(document.querySelector(`#${droppedEl}`));
+
+	}
+
+	function allowDrop(event) {
+		// turn off the default browser behaviour -> follow our instructions instead of what a browser would normally do on drop
+		event.preventDefault();
+		console.log('dropped on me');
+	}
+
 	bottomNav.forEach(thumb => thumb.addEventListener("click", changeBgImg));
+
+	// listen for the dragstarted event on the puzzlePieces
+	puzzlePieces.forEach(piece => piece.addEventListener("dragstart", dragStarted));
+
+	// add event handling for the drop zones (drag over and drop)
+	dropZones.forEach(zone => {
+		zone.addEventListener("dragover", allowDragOver);
+		zone.addEventListener("drop", allowDrop);
+	});
 
 })();
